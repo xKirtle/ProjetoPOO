7c36bde44079048b6cdb6fe7192340ebe36f0038
xKirtle <xkirtle26@gmail.com>
2021-11-20 19:10:11 +0000

    IDK

--- src/pt/iul/poo/firefight/starterpack/Fire.java
+++ src/pt/iul/poo/firefight/starterpack/Fire.java
@@ -4,7 +4,7 @@ import pt.iul.ista.poo.gui.ImageTile;
 import pt.iul.ista.poo.utils.Point2D;
 
 //Esta classe de exemplo esta' definida de forma muito basica, sem relacoes de heranca
-//Tem atributos e metodos repetidos em relacao ao que est· definido noutras classes 
+//Tem atributos e metodos repetidos em relacao ao que est√° definido noutras classes 
 //Isso sera' de evitar na versao a serio do projeto
 
 public class Fire implements ImageTile {
